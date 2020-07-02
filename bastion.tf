a3902f45a279f2daa72149ae17584b390d355fd2
Rodrigo Noya <rodrinoya.1@gmail.com>
2020-07-02 18:47:12 -0300

    provisioner splitted

--- bastion.tf
+++ bastion.tf
@@ -12,23 +12,3 @@ resource "openstack_compute_instance_v2" "bastion-terraform" {
     name = "${openstack_networking_network_v2.terra-net.name}"
   }
 }
-
-#si sacas el bastion devolve esto a lb.tf AFUERA del recurso de computo
-resource "null_resource" "remoteexec" {
-  #count = 1 
-  connection {
-      type = "ssh"
-      user = "ubuntu"
-      private_key = file("/home/roi/.ssh/id_rsa")
-      #host = openstack_networking_floatingip_v2.fip_lb.address
-      host = openstack_networking_floatingip_v2.fip_bastion.address
-      agent = true
-  }
-  provisioner "remote-exec" {
-    inline = [
-      #"sudo apt-get update",
-      "touch Here",
-      "ssh -o StrictHostKeyChecking=no ubuntu@${openstack_compute_instance_v2.wp-terraform.access_ip_v4} 'touch now_here'"
-    ]
-  }
-}
